b5c7a6aa31f8c21755f20b6c15853877add829b9
asa.the.hawk <45080213+asathehawk@users.noreply.github.com>
2020-01-16 09:17:42 +0100

    Create airplane.js

---
+++ airplane.js
@@ -0,0 +1,44 @@
+export let availableAirplanes = [
+    {
+      name: 'AeroJet',
+      fuelCapacity: 800,
+      availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+      maxSpeed: 1200,
+      minSpeed: 300
+    },
+    {
+      name: 'SkyJet',
+      fuelCapacity: 500,
+      availableStaff: ['pilots', 'flightAttendants'],
+      maxSpeed: 800,
+      minSpeed: 200
+    }
+  ]
+ 
+ export let flightRequirements = {
+   requiredStaff: 4,
+   requiredSpeedRange : 700
+ }
+ 
+ export function meetsStaffRequirements(availableStaff, requiredStaff) {
+   if (availableStaff.length >= requiredStaff) {
+     return true;
+   }
+   else {
+     return false;
+   }
+ }
+
+export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+  let range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+//this has become an obsolete variable export statement: export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+//export { availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs };
+//export default flightRequirements
